041f1e71321d05ad1027891051df8e1aea935599
Razielini <razielco@gmail.com>
2020-06-07 00:32:14 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './styles/GlobalStyles'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards/index'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo/index'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <>
-    <Logo />
-    <GlobalStyles />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={1} />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log('detailId::', detailId)
+
+  return (
+    <>
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <>
+            <GlobalStyles />
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={1} />
+          </>
+      }
+    </>
+  )
+}
