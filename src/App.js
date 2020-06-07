2b3cc6301e3c64d5af8ece496d4df74c0e9e8c77
Razielini <razielco@gmail.com>
2020-06-06 23:41:46 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyles } from './styles/GlobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards/index'
+import { Logo } from './components/Logo/index'
+
+export const App = () => (
+  <>
+    <Logo />
+    <GlobalStyles />
+    <ListOfCategories />
+    <ListOfPhotoCards />
+  </>
+)
---
+++ src/components/ListOfCategories/index.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import { Category } from '../Category'
+import { List, Item } from './styles'
+
+const useCategoriesData = () => {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(function () {
+    setLoading(true)
+    window.fetch('https://petgram-server.razielini.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+        setLoading(false)
+      })
+  }, [])
+
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
+    </List>
+  )
+
+  if (loading) return 'Cargando'
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+  )
+}
