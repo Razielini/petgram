c51d6e291def51f0ba6850a95468a4fb6678aa5b
Razielini <razielco@gmail.com>
2020-06-06 23:55:43 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import ApolloCLient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import { App } from './App'
 
-ReactDom.render(<App />, document.getElementById('app'))
+const client = new ApolloCLient({
+  uri: 'https://petgram-server.razielini.now.sh/graphql'
+})
+
+ReactDom.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app'))
