b92f3fb45b54ad379048c7c5fe2b2ecc774bc13b
Razielini <razielco@gmail.com>
2020-06-08 16:37:06 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useContext, Suspense } from 'react'
+import { Router, Redirect } from '@reach/router'
 import { GlobalStyles } from './styles/GlobalStyles'
+import { Context } from './Context'
+
 import { Logo } from './components/Logo/index'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
-import { Home } from './pages/Home'
-import { Router } from '@reach/router'
+import { NavBar } from './components/NavBar'
+
 import { Detail } from './pages/Detail'
+import { Home } from './pages/Home'
+// import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
+
+const Favs = React.lazy(() => import('./pages/Favs'))
 
 export const App = () => {
-
+  const { isAuth } = useContext(Context)
   return (
-    <>
+    <Suspense fallback={<div />}>
       <GlobalStyles />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-    </>
+      <NavBar />
+    </Suspense>
   )
 }
