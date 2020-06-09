b9cce89b4cd9d2a9740dec81e90ef835321a2c0e
Razielini <razielco@gmail.com>
2020-06-09 14:23:39 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ import ReactDom from 'react-dom'
 import ApolloCLient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import { App } from './App'
+import Context from './Context'
 
 const client = new ApolloCLient({
-  uri: 'https://petgram-server.razielini.now.sh/graphql'
+  uri: 'https://petgram-server.razielini.now.sh/graphql',
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: error => {
+    const { networkError } = error
+    if (networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 })
 
 ReactDom.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Context.Provider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Context.Provider>,
   document.getElementById('app'))
