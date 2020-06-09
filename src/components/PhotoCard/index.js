ccc2e3205e8e191005fdec6ade91bbc42eca448d
Razielini <razielco@gmail.com>
2020-06-09 14:23:04 -0600

    Update index.js

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,37 +1,35 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { ImgWrapper, Img, Article } from './styles'
-import { useLocalStorage } from '../../hooks/setLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton/index'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
+import { Link } from '@reach/router'
+import PropTypes from 'prop-types'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [liked, setliked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, ref] = useNearScreen()
 
   return (
     <Article ref={ref}>
       {
         show && <>
-          <a href={`/?detail=${id}`}>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
-          </a>
+          </Link>
           <ToggleLikeMutation>
             {
               (toggleLike) => {
                 const handleFavClick = () => {
-                  !liked && toggleLike(
+                  toggleLike(
                     {
                       variables: {
                         input: { id }
                       }
                     }
                   )
-                  setliked(!liked)
                 }
                 return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
               }
@@ -41,4 +39,21 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       }
     </Article>
   )
+}
+
+PhotoCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  likes: function (props, propName, componentName) {
+    const propValue = props[propName]
+
+    if (propValue === undefined) {
+      return new Error(`${propName}: Value must be defined`)
+    }
+
+    if (propValue < 0) {
+      return new Error(`${propName}: Value must be greather than 0`)
+    }
+  }
 }
